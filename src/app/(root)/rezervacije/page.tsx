dfed6c0dfc4394d7230f8d91eb4f6bc6fd048a0e
kosovic-creator <drasko.kosovic@icloud.com>
2025-04-06 00:19:48 +0200

    init

--- src/app/(root)/rezervacije/page.tsx
+++ src/app/(root)/rezervacije/page.tsx
@@ -2,13 +2,15 @@ import { auth } from '@/lib/auth';
 import { redirect } from 'next/navigation';
 import React from 'react';
 
+
  const Rezervacija = async () => {
       const session = await auth();
       console.log ('SESIJA JE  ',session?.user?.name);
       if (session?.user?.name!=='ADMIN') redirect("/not-authorized");
     return (
-        <div className='bg-gray-800 rounded-lg p-4 text-center mb-6 md:ml-6 text-white'>
+        <div className='bg-gray-300 rounded-lg p-4 text-center mb-6 md:ml-6 text-black'>
 <h5>Rezervacije</h5>
+
         </div>
     );
 }
